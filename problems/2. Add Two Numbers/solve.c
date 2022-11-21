33b4343abbedb7f0c48c5864f0c433e5be21193f
David Bergh <berghdavid@hotmail.com>
2022-11-21 15:38:16 +0100

    Init A2

---
+++ problems/2. Add Two Numbers/solve.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct ListNode ListNode;
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+
+}
